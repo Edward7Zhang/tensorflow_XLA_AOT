86683f6be2da79b720ee608d03da80a66a184432
Edward7Zhang <super7edwardzhang@gmail.com>
2019-07-19 17:38:33 +0800

    compile for linux-pc-x86_64 plantform .h .o .so

--- tensorflow/compiler/aot/custom/custom_interface.cc
+++ tensorflow/compiler/aot/custom/custom_interface.cc
@@ -2,7 +2,8 @@
 #define EIGEN_USE_CUSTOM_THREAD_POOL
 
 #include <iostream>
-#include "out.h"
+#include <cstdio>
+#include "custom_interface_lib.h"
 #include "third_party/eigen3/unsupported/Eigen/CXX11/Tensor"
 
 extern "C" int run(float* input, float* output, int input_size, int output_size){
@@ -15,8 +16,31 @@ extern "C" int run(float* input, float* output, int input_size, int output_size)
     std::copy(input, input + input_size, classifier.arg0_data());
     auto ok = classifier.Run();
     if (not ok) return -1;
+    std::cout << "input:";
+    std::cout << input << std::endl;
+
+    std::cout << "input_size:";
+    std::cout << input_size << std::endl;
+
+    std::cout << "classifier.arg0_data():";
+    std::cout << classifier.arg0_data() << std::endl;
+
+    std::cout << "output:";
     std::cout << output << std::endl;
+
+    std::cout << "output_size:";
     std::cout << output_size << std::endl;
+
+    std::cout << "result0_data():";
+    std::cout << classifier.result0_data() << std::endl;
+
+    for(int i = 0; i < 30; i++){
+        std::cout << "restul0_";
+        std::cout << i;
+        std::cout << " : ";
+        std::cout << classifier.result0(0,i) << std::endl;
+    }
+
     std::copy(classifier.result0_data(), classifier.result0_data() + output_size, output);
     return 0;
 }
--- tensorflow/compiler/aot/custom/predict_model.py
+++ tensorflow/compiler/aot/custom/predict_model.py
@@ -1,19 +1,47 @@
 import numpy as np
 from keras.preprocessing import image
+import tensorflow as tf
 
-lib_custom_interface = np.ctypeslib.load_library('lib_custom_interface', '/home/majortom/WORKSPACE/tensorflow_XLA_AOT/tensorflow/compiler/aot/custom')
+lib_custom_interface = np.ctypeslib.load_library('lib_custom_interface', '/home/majortom/WORKSPACE/tensorflow_XLA_AOT/bazel-bin/tensorflow/compiler/aot/custom')
 lib_custom_interface.run.argtypes = [
-    np.ctypeslib.ndpointer(np.float32, ndim=4, shape=(1, 160, 160, 3)),
-    np.ctypeslib.ndpointer(np.float32, ndim=2, shape=(1, 30)),
+    np.ctypeslib.ndpointer(np.float32, ndim=4, shape=(1, 160, 160, 3), flags=('c', 'a')),
+    np.ctypeslib.ndpointer(np.float32, ndim=2, shape=(1, 30), flags=('c', 'a', 'w')),
     np.ctypeslib.ctypes.c_int,
     np.ctypeslib.ctypes.c_int]
 
 
-def predict(In):
-    In = np.require(In, np.float32)
-    Out = np.require(np.zeros((1, 30)), np.float32)
+def predict_so(In):
+    In = np.require(In, np.float32, ('c', 'a'))
+    Out = np.require(np.zeros((1, 30)), np.float32, ('c', 'a', 'w'))
     lib_custom_interface.run(In, Out, In.size, Out.size)
     return Out
+
+def predict_pb(x):
+    with tf.gfile.GFile('./frozen_custom_010.pb','rb') as f:
+        graph_def = tf.compat.v1.GraphDef()
+        graph_def.ParseFromString(f.read())
+
+        with tf.Graph().as_default() as graph:
+            tf.import_graph_def(
+                graph_def,
+                input_map=None,
+                return_elements=None,
+                name="",
+                op_dict=None,
+                producer_op_list=None
+            )
+            with tf.Session() as sess:
+                input = graph.get_tensor_by_name('input:0')
+
+                feed_dict = {
+                    input.name : x
+                }
+
+                output_prediction = graph.get_tensor_by_name('MobilenetV2/Predictions/Reshape_1:0')
+                output_prediction = sess.run(output_prediction, feed_dict)
+                return output_prediction
+
+
 
 
 image_path = input()
@@ -22,6 +50,9 @@ x = image.img_to_array(image.load_img(image_path, target_size=(160, 160)))
 x = x[None, ...]
 # x = preprocess_input(x)
 # print(x)
-y = predict(x)
-print(y[0])
+z = predict_pb(x)
+
+print(z[0])
+y = predict_so(x)
+# print(y[0])
 # print(decode_predictions(y)[0])
